3e59c46543f05eb5209d241bc6aa45a0fdfe3712
Sergio Oporto <ser.op.te@gmail.com>
2021-03-20 15:16:56 -0400

    Generate entities and db context using Scaffold-DbContext.

---
+++ API/src/TheGreatPizzaTest.Infrastructure/Data/Entities/PizzaTopping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TheGreatPizzaTest.Core.Entities
+{
+    public partial class PizzaTopping
+    {
+        public int PizzaId { get; set; }
+        public int IngredientId { get; set; }
+
+        public virtual Ingredient Ingredient { get; set; }
+        public virtual Pizza Pizza { get; set; }
+    }
+}
