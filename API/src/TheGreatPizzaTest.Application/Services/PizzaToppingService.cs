e58535b0caa86d723f45349ae5c2c0188674b593
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 15:12:12 -0400

    Throw exceptions when managing pizza toppings.

--- API/src/TheGreatPizzaTest.Application/Services/PizzaToppingService.cs
+++ API/src/TheGreatPizzaTest.Application/Services/PizzaToppingService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TheGreatPizzaTest.Application.DTOs;
+using TheGreatPizzaTest.Application.Exceptions;
 using TheGreatPizzaTest.Application.Interfaces;
 using TheGreatPizzaTest.Core.Entities;
 using TheGreatPizzaTest.Core.Repositories;
@@ -15,15 +16,28 @@ namespace TheGreatPizzaTest.Application.Services
     {
         private readonly IMapper _mapper;
         private readonly IPizzaToppingRepository _pizzaToppingRepository;
+        private readonly IPizzaRepository _pizzaRepository;
+        private readonly IIngredientRepository _ingredientRepository;
 
-        public PizzaToppingService(IMapper mapper, IPizzaToppingRepository pizzaToppingRepository)
+        public PizzaToppingService(
+            IMapper mapper, 
+            IPizzaToppingRepository pizzaToppingRepository, 
+            IPizzaRepository pizzaRepository,
+            IIngredientRepository ingredientRepository)
         {
             _mapper = mapper;
             _pizzaToppingRepository = pizzaToppingRepository;
+            _pizzaRepository = pizzaRepository;
+            _ingredientRepository = ingredientRepository;
         }
 
         public async Task<IEnumerable<IngredientDto>> GetToppingsForPizza(int pizzaId)
         {
+            var pizza = await _pizzaRepository.GetByIdAsync(pizzaId);
+            if (pizza == null)
+            {
+                throw new EntityCantBeLoadedException<Pizza>(pizzaId.ToString());
+            }
             var toppings = await _pizzaToppingRepository.GetToppingsFromPizzaAsync(pizzaId);
             var ingredients = toppings.Select(pt => pt.Ingredient);
             return _mapper.Map<IEnumerable<IngredientDto>>(ingredients);
@@ -32,12 +46,32 @@ namespace TheGreatPizzaTest.Application.Services
 
         public async Task AddToppingToPizza(int pizzaId, int ingredientId)
         {
+            var pizza = await _pizzaRepository.GetByIdAsync(pizzaId);
+            if (pizza == null)
+            {
+                throw new EntityCantBeLoadedException<Pizza>(pizzaId.ToString());
+            }
+            var ingredient = await _ingredientRepository.GetByIdAsync(ingredientId);
+            if (ingredient == null)
+            {
+                throw new EntityCantBeLoadedException<Ingredient>(ingredientId.ToString());
+            }
+            var topping = await _pizzaToppingRepository.GetToppingAsync(pizzaId, ingredientId);
+            if (topping != null)
+            {
+                throw new EntityAlreadyExistsException<PizzaTopping>($"{{{pizzaId}, {ingredientId}}}");
+            }
             await _pizzaToppingRepository.AddAsync(new PizzaTopping() { PizzaId = pizzaId, IngredientId = ingredientId });
         }
 
         public async Task DeleteToppingFromPizza(int pizzaId, int ingredientId)
         {
-            await _pizzaToppingRepository.DeleteAsync(new PizzaTopping() { PizzaId = pizzaId, IngredientId = ingredientId });
+            var topping = await _pizzaToppingRepository.GetToppingAsync(pizzaId, ingredientId);
+            if (topping == null)
+            {
+                throw new EntityCantBeLoadedException<PizzaTopping>($"{{{pizzaId}, {ingredientId}}}");
+            }
+            await _pizzaToppingRepository.DeleteAsync(topping);
         }
     }
 }
