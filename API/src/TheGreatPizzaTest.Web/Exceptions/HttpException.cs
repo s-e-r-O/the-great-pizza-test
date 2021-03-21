9cb799dd7438115059687ef7b7a390940942192a
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 13:13:29 -0400

    Add global error exception handler.

---
+++ API/src/TheGreatPizzaTest.Web/Exceptions/HttpException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TheGreatPizzaTest.Web.Exceptions
+{
+    public class HttpException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public HttpException(HttpStatusCode statusCode, string message)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
---
+++ API/src/TheGreatPizzaTest.Web/Models/ErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Net;
+
+namespace TheGreatPizzaTest.Web.Models
+{
+    public class ErrorResponse
+    {
+        public HttpStatusCode Status { get; set; } = HttpStatusCode.InternalServerError;
+        public string Message { get; set; } = "An unexpected error occurred.";
+
+        public string ToJsonString()
+        {
+            return JsonConvert.SerializeObject(this, new JsonSerializerSettings() {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+        }
+    }
+}
