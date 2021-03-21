f7f617f3321e8f9c1aadffb8ce5d5342defc5fe2
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 09:41:02 -0400

    Add base ingredient service.

---
+++ API/src/TheGreatPizzaTest.Application/Interfaces/IIngredientService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Application.DTOs;
+
+namespace TheGreatPizzaTest.Application.Interfaces
+{
+    public interface IIngredientService
+    {
+        Task<IEnumerable<IngredientDto>> GetIngredientsAsync();
+    }
+}
---
+++ API/src/TheGreatPizzaTest.Application/Services/IngredientService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Application.DTOs;
+using TheGreatPizzaTest.Application.Interfaces;
+using TheGreatPizzaTest.Core.Repositories;
+
+namespace TheGreatPizzaTest.Application.Services
+{
+    public class IngredientService : IIngredientService
+    {
+        private readonly IMapper _mapper;
+        private readonly IIngredientRepository _ingredientRepository;
+        public IngredientService(IMapper mapper, IIngredientRepository ingredientRepository)
+        {
+            _mapper = mapper;
+            _ingredientRepository = ingredientRepository;
+        }
+
+        public async Task<IEnumerable<IngredientDto>> GetIngredientsAsync()
+        {
+            var ingredients = await _ingredientRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<IngredientDto>>(ingredients);
+        }
+    }
+}
