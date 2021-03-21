d8eba76dec1dbe1c30c24f541dde3e8e94c4c769
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 12:05:18 -0400

    Add pizza topping service.

---
+++ API/src/TheGreatPizzaTest.Application/Interfaces/IPizzaToppingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Application.DTOs;
+
+namespace TheGreatPizzaTest.Application.Interfaces
+{
+    public interface IPizzaToppingService
+    {
+        Task<IEnumerable<IngredientDto>> GetToppingsForPizza(int pizzaId);
+        Task AddToppingToPizza(int pizzaId, int ingredientId);
+        Task DeleteToppingFromPizza(int pizzaId, int ingredientId);
+    }
+}
---
+++ API/src/TheGreatPizzaTest.Application/Services/PizzaToppingService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Application.DTOs;
+using TheGreatPizzaTest.Application.Interfaces;
+using TheGreatPizzaTest.Core.Entities;
+using TheGreatPizzaTest.Core.Repositories;
+
+namespace TheGreatPizzaTest.Application.Services
+{
+    public class PizzaToppingService : IPizzaToppingService
+    {
+        private readonly IMapper _mapper;
+        private readonly IPizzaToppingRepository _pizzaToppingRepository;
+
+        public PizzaToppingService(IMapper mapper, IPizzaToppingRepository pizzaToppingRepository)
+        {
+            _mapper = mapper;
+            _pizzaToppingRepository = pizzaToppingRepository;
+        }
+
+        public async Task<IEnumerable<IngredientDto>> GetToppingsForPizza(int pizzaId)
+        {
+            var toppings = await _pizzaToppingRepository.GetToppingsFromPizzaAsync(pizzaId);
+            var ingredients = toppings.Select(pt => pt.Ingredient);
+            return _mapper.Map<IEnumerable<IngredientDto>>(ingredients);
+
+        }
+
+        public async Task AddToppingToPizza(int pizzaId, int ingredientId)
+        {
+            await _pizzaToppingRepository.AddAsync(new PizzaTopping() { PizzaId = pizzaId, IngredientId = ingredientId });
+        }
+
+        public async Task DeleteToppingFromPizza(int pizzaId, int ingredientId)
+        {
+            await _pizzaToppingRepository.DeleteAsync(new PizzaTopping() { PizzaId = pizzaId, IngredientId = ingredientId });
+        }
+    }
+}
