82ac4af2fc3ca6d937fa6ecf9330a69671ee3491
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 19:07:46 -0400

    Add data services

---
+++ Web/src/app/data/services/ingredient-data.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Ingredient } from '@data/types/models';
+import { Observable } from 'rxjs';
+
+const routes = {
+  ingredients: 'ingredients',
+  ingredient: (ingredientId: number) => `ingredients/${ingredientId}`,
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IngredientDataService {
+  constructor(private http: HttpClient) {}
+
+  public getAll(): Observable<Ingredient[]> {
+    return this.http.get<Ingredient[]>(routes.ingredients);
+  }
+
+  public getById(id: number): Observable<Ingredient> {
+    return this.http.get<Ingredient>(routes.ingredient(id));
+  }
+
+  public add(ingredient: Ingredient): Observable<Ingredient> {
+    return this.http.post<Ingredient>(routes.ingredients, ingredient);
+  }
+
+  public update(ingredient: Ingredient): Observable<any> {
+    return this.http.put(routes.ingredient(ingredient.id), ingredient);
+  }
+
+  public delete(ingredientId: number): Observable<any> {
+    return this.http.delete(routes.ingredient(ingredientId));
+  }
+}
