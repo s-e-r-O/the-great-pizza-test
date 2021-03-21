e24fed82a2b3c665951102bb55d0b0a158afabfe
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 12:05:05 -0400

    Add pizza topping repository.

---
+++ API/src/TheGreatPizzaTest.Core/Repositories/IPizzaToppingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Core.Entities;
+using TheGreatPizzaTest.Core.Repositories.Base;
+
+namespace TheGreatPizzaTest.Core.Repositories
+{
+    public interface IPizzaToppingRepository : IRepository<PizzaTopping>
+    {
+        Task<IReadOnlyList<PizzaTopping>> GetToppingsFromPizzaAsync(int pizzaId);
+    }
+}
--- API/src/TheGreatPizzaTest.Web/Startup.cs
+++ API/src/TheGreatPizzaTest.Web/Startup.cs
@@ -54,6 +54,7 @@ namespace Presentation
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped<IPizzaRepository, PizzaRepository>();
             services.AddScoped<IIngredientRepository, IngredientRepository>();
+            services.AddScoped<IPizzaToppingRepository, PizzaToppingRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
