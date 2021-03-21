8f7e71e077cf4df5177dbc413a0d3111580f6ced
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 00:15:10 -0400

    Add entities DTOs.

---
+++ API/src/TheGreatPizzaTest.Application/DTOs/IngredientDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGreatPizzaTest.Application.DTOs
+{
+    public class IngredientDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
