b41246e582b16c9529da85ff5958ad308c42c3cd
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 00:35:41 -0400

    Add a pizza controller to get all pizzas.

---
+++ API/src/TheGreatPizzaTest.Web/Controllers/PizzasController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Application.DTOs;
+using TheGreatPizzaTest.Application.Interfaces;
+
+namespace TheGreatPizzaTest.Web.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzasController : ControllerBase
+    {
+        private readonly IPizzaService _pizzaService;
+
+        public PizzasController(IPizzaService pizzaService)
+        {
+            _pizzaService = pizzaService;
+        }
+
+        /// <summary>
+        /// Get all pizzas
+        /// </summary>
+        /// <remarks>
+        /// Get a list of all of the pizzas, each with a list of its ingredients.
+        /// </remarks>
+        /// <returns>A list of all of the pizzas, each with a list of its ingredients</returns>
+        /// <response code="200">Returns a list of all of the pizzas, each with a list of its ingredients</response>
+        [HttpGet]
+        public async Task<IEnumerable<PizzaDto>> GetAsync()
+        {
+            return await _pizzaService.GetPizzasAsync();
+        }
+    }
+}
