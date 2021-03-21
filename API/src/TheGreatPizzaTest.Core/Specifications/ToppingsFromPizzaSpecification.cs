b5fa726311b954e362f2f5acf179ccbab28630bf
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 12:05:05 -0400

    Add pizza topping repository.

---
+++ API/src/TheGreatPizzaTest.Core/Specifications/ToppingsFromPizzaSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Core.Entities;
+using TheGreatPizzaTest.Core.Specifications.Base;
+
+namespace TheGreatPizzaTest.Core.Specifications
+{
+    public class ToppingsFromPizzaSpecification : BaseSpecification<PizzaTopping>
+    {
+        public ToppingsFromPizzaSpecification(int pizzaId) : base(p => p.PizzaId == pizzaId)
+        {
+            AddInclude(p => p.Ingredient);
+        }
+    }
+}
---
+++ API/src/TheGreatPizzaTest.Infrastructure/Repositories/PizzaToppingRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Core.Entities;
+using TheGreatPizzaTest.Core.Repositories;
+using TheGreatPizzaTest.Core.Specifications;
+using TheGreatPizzaTest.Infrastructure.Data;
+using TheGreatPizzaTest.Infrastructure.Repositories.Base;
+
+namespace TheGreatPizzaTest.Infrastructure.Repositories
+{
+    public class PizzaToppingRepository : Repository<PizzaTopping>, IPizzaToppingRepository
+    {
+        public PizzaToppingRepository(TheGreatPizzaTestDBContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IReadOnlyList<PizzaTopping>> GetToppingsFromPizzaAsync(int pizzaId)
+        {
+            var spec = new ToppingsFromPizzaSpecification(pizzaId);
+            return await GetAsync(spec);
+        }
+    }
+}
