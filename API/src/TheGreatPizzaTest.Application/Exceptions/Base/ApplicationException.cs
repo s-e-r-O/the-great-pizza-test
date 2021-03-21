67da5d6b1aa98ee7f5140e9b9824279e6ebecafa
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 15:08:32 -0400

    Add application exceptions

---
+++ API/src/TheGreatPizzaTest.Application/Exceptions/Base/ApplicationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGreatPizzaTest.Application.Exceptions.Base
+{
+    public class ApplicationException : Exception
+    {
+        internal ApplicationException(string message) : base(message)
+        {
+        }
+    }
+}
