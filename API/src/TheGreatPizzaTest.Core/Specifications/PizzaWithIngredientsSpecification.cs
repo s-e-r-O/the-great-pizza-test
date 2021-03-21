795035d219f7f442b123bc60ba2ac5394a2881bd
Sergio Oporto <ser.op.te@gmail.com>
2021-03-20 23:34:27 -0400

    Add query specification to get pizzas with their ingredients.

---
+++ API/src/TheGreatPizzaTest.Core/Specifications/PizzaWithIngredientsSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Core.Entities;
+using TheGreatPizzaTest.Core.Specifications.Base;
+
+namespace TheGreatPizzaTest.Core.Specifications
+{
+    public class PizzaWithIngredientsSpecification : BaseSpecification<Pizza>
+    {
+        public PizzaWithIngredientsSpecification() : base(null)
+        {
+            AddInclude(p => p.Ingredients);
+        }
+    }
+}
