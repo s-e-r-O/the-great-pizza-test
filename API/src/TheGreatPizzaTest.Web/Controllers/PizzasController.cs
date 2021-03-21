3b80e159fb657a10acaa65cffe473aa0a08aa3a8
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 16:19:52 -0400

    Add XML comments to API endpoints.

--- API/src/TheGreatPizzaTest.Web/Controllers/PizzasController.cs
+++ API/src/TheGreatPizzaTest.Web/Controllers/PizzasController.cs
@@ -9,9 +9,13 @@ using TheGreatPizzaTest.Application.DTOs;
 using TheGreatPizzaTest.Application.Interfaces;
 using TheGreatPizzaTest.Application.Models;
 using TheGreatPizzaTest.Web.Exceptions;
+using TheGreatPizzaTest.Web.Models;
 
 namespace TheGreatPizzaTest.Web.Controllers
 {
+    /// <summary>
+    /// Manage and request Pizzas and Pizza Toppings 
+    /// </summary>
     [Produces("application/json")]
     [ApiController]
     [Route("[controller]")]
@@ -29,18 +33,25 @@ namespace TheGreatPizzaTest.Web.Controllers
         /// <summary>
         /// Get all pizzas
         /// </summary>
-        /// <remarks>
-        /// Get a list of all of the pizzas, each with a list of its ingredients.
-        /// </remarks>
-        /// <returns>A list of all of the pizzas, each with a list of its ingredients</returns>
-        /// <response code="200">Returns a list of all of the pizzas, each with a list of its ingredients</response>
+        /// <remarks>Returns a list of all of the pizzas and their respective ingredients</remarks>
+        /// <response code="200">Successsful operation</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<PizzaDto>>> GetAll()
         {
             return Ok(await _pizzaService.GetPizzasAsync());
         }
 
+        /// <summary>
+        /// Find pizza by ID
+        /// </summary>
+        /// <remarks>Returns the pizza specified and its respective ingredients</remarks>
+        /// <param name="id">ID of the pizza to fetch</param>
+        /// <response code="200">Successfull operation</response>
+        /// <response code="404">Pizza not found</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorResponse))]
         public async Task<ActionResult<PizzaDto>> GetById([FromRoute] int id)
         {
             var pizza = await _pizzaService.GetPizzaByIdAsync(id);
@@ -51,14 +62,31 @@ namespace TheGreatPizzaTest.Web.Controllers
             return Ok(pizza);
         }
 
+        /// <summary>
+        /// Create pizza
+        /// </summary>
+        /// <param name="pizza">Pizza to be added</param>
+        /// <response code="200">Successfull operation</response>
+        /// <response code="400">Invalid input</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ErrorResponse))]
         public async Task<ActionResult<PizzaDto>> PostPizza([FromBody] CreatePizzaModel pizza)
         {
             var newPizza = await _pizzaService.Create(pizza);
             return CreatedAtAction(nameof(GetById), new { id = newPizza.Id }, newPizza);
         }
 
+        /// <summary>
+        /// Update an existing pizza
+        /// </summary>
+        /// <param name="id">ID of the pizzato update</param>
+        /// <param name="pizza">Updated pizza object</param>
+        /// <response code="200">Successfull operation</response>
+        /// <response code="400">Invalid pizza supplied</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ErrorResponse))]
         public async Task<ActionResult> PutPizza([FromRoute] int id, [FromBody] UpdatePizzaModel pizza)
         {
             if (id != pizza.Id)
@@ -69,15 +97,29 @@ namespace TheGreatPizzaTest.Web.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Delete pizza
+        /// </summary>
+        /// <param name="id">ID of the pizza to delete</param>
+        /// <response code="200">Successfull operation</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> DeletePizza([FromRoute] int id)
         {
             await _pizzaService.Delete(id);
             return Ok();
         }
 
-
-        [HttpGet("{pizzaId}/toppings")]
+        /// <summary>
+        /// Find toppings by pizza ID
+        /// </summary>
+        /// <remarks>Returns a list of ingredients from a the specified pizza</remarks>
+        /// <param name="pizzaId">ID of the pizza whose ingredients we want to fetch</param>
+        /// <response code="200">Successfull operation</response>
+        /// <response code="404">Pizza not found</response>
+        [HttpGet("{pizzaId}/Toppings")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorResponse))]
         public async Task<ActionResult<IEnumerable<IngredientDto>>> GetToppingsForPizza([FromRoute] int pizzaId)
         {
             var pizza = await _pizzaService.GetPizzaByIdAsync(pizzaId);
@@ -88,15 +130,30 @@ namespace TheGreatPizzaTest.Web.Controllers
 
             return Ok(await _pizzaToppingService.GetToppingsForPizza(pizzaId));
         }
-        
-        [HttpPut("{pizzaId}/toppings/{ingredientId}")]
+
+        /// <summary>
+        /// Add topping to pizza
+        /// </summary>
+        /// <remarks>The pizza and the ingredient need to be created previously</remarks>
+        /// <param name="pizzaId">ID of the pizza</param>
+        /// <param name="ingredientId">ID of the ingredient</param>
+        /// <response code="200">Successfull operation</response>
+        [HttpPut("{pizzaId}/Toppings/{ingredientId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> AddToppingToPizza([FromRoute] int pizzaId, [FromRoute] int ingredientId)
         {
             await _pizzaToppingService.AddToppingToPizza(pizzaId, ingredientId);
             return Ok();
         }
-        
-        [HttpDelete("{pizzaId}/toppings/{ingredientId}")]
+
+        /// <summary>
+        /// Delete topping from pizza
+        /// </summary>
+        /// <param name="pizzaId">ID of the pizza</param>
+        /// <param name="ingredientId">ID of the ingredient</param>
+        /// <response code="200">Successfull operation</response>
+        [HttpDelete("{pizzaId}/Toppings/{ingredientId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> DeleteToppingFromPizza([FromRoute] int pizzaId, [FromRoute] int ingredientId)
         {
             await _pizzaToppingService.DeleteToppingFromPizza(pizzaId, ingredientId);
