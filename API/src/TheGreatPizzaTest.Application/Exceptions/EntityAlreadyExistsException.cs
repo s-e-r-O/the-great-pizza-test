e560c572abcf745d876d173ceea33084c662e6c0
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 15:08:32 -0400

    Add application exceptions

---
+++ API/src/TheGreatPizzaTest.Application/Exceptions/EntityAlreadyExistsException.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Application.Exceptions.Base;
+using TheGreatPizzaTest.Core.Entities.Base;
+
+namespace TheGreatPizzaTest.Application.Exceptions
+{
+    public class EntityAlreadyExistsException<T> : ApplicationException where T : Entity
+    {
+        internal EntityAlreadyExistsException(string id) : base($"{typeof(T).Name} with id {id} already exists.")
+        {
+        }
+    }
+}
---
+++ API/src/TheGreatPizzaTest.Application/Exceptions/EntityCantBeLoadedException.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGreatPizzaTest.Application.Exceptions.Base;
+using TheGreatPizzaTest.Core.Entities.Base;
+
+namespace TheGreatPizzaTest.Application.Exceptions
+{
+    public class EntityCantBeLoadedException<T> : ApplicationException where T : Entity
+    {
+        public EntityCantBeLoadedException(string id) : base($"{typeof(T).Name} with id {id} could not be loaded.")
+        {
+
+        }
+    }
+}
