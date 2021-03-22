e2218ea317e0d0ee89d9d46c1d71a8b2f6080199
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 21:18:13 -0400

    Import stores in core module.

--- Web/src/app/core/core.module.ts
+++ Web/src/app/core/core.module.ts
@@ -4,13 +4,22 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '@env';
-
+import * as fromPizzas from '@modules/pizzas/store';
+import * as fromIngredients from '@modules/ingredients/store';
+import { PizzasEffects } from '@modules/pizzas/store/effects';
+import { IngredientsEffects } from '@modules/ingredients/store/effects';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
+    StoreModule.forRoot(
+      {
+        [fromPizzas.featureKey]: fromPizzas.reducers,
+        [fromIngredients.featureKey]: fromIngredients.reducers,
+      },
+      {}
+    ),
+    EffectsModule.forRoot([PizzasEffects, IngredientsEffects]),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production,
