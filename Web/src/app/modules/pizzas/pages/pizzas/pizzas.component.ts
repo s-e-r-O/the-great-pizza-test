29f372a098e061212325561c32b95ce65761a100
Sergio Oporto <ser.op.te@gmail.com>
2021-03-21 23:02:21 -0400

    Trigger pizza form dialog.

--- Web/src/app/modules/pizzas/pages/pizzas/pizzas.component.ts
+++ Web/src/app/modules/pizzas/pages/pizzas/pizzas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { PizzaVM } from '@data/types/view-models';
+import { PizzaDialogComponent } from '@modules/pizzas/components';
 import * as fromPizzas from '@modules/pizzas/store';
 import { PizzaApiActions } from '@modules/pizzas/store';
 import { Store } from '@ngrx/store';
@@ -11,11 +13,23 @@ import { Observable } from 'rxjs';
 export class PizzasComponent implements OnInit {
   pizzas$: Observable<PizzaVM[]>;
 
-  constructor(private store: Store<fromPizzas.State>) {
+  constructor(
+    private store: Store<fromPizzas.State>,
+    private dialog: MatDialog
+  ) {
     this.pizzas$ = this.store.select(fromPizzas.selectAllPizzas);
   }
 
   ngOnInit(): void {
     this.store.dispatch(PizzaApiActions.loadPizzas());
+  }
+
+  onAdd(): void {
+    const dialogRef = this.dialog.open(PizzaDialogComponent, { data: {} });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!!result) {
+        this.store.dispatch(PizzaApiActions.addPizza({ pizza: result }));
+      }
+    });
   }
 }
