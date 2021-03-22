68283b1e95cbd996bc64580c8f9b90636e3976a6
Sergio Oporto <ser.op.te@gmail.com>
2021-03-22 11:44:41 -0400

    Add material module

--- Web/src/app/shared/shared.module.ts
+++ Web/src/app/shared/shared.module.ts
@@ -1,29 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './modules/material/material.module';
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDialogModule,
-    MatInputModule,
-    MatCardModule,
-  ],
-  exports: [
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDialogModule,
-    MatInputModule,
-    MatCardModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, MaterialModule],
+  exports: [ReactiveFormsModule, MaterialModule],
 })
 export class SharedModule {}
