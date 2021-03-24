1ed8966a480b2ab772556797c86f215a1a2ea6b6
Sergio Oporto <ser.op.te@gmail.com>
2021-03-24 18:32:12 -0400

    Split SQL query into schema and data

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,4 +1,4 @@
-﻿-- Create a database "TheGreatPizzaTestDB".
+-- Create a database "TheGreatPizzaTestDB".
 -- If it already exists, we drop it.
 
 USE master;
@@ -40,16 +40,4 @@ CREATE NONCLUSTERED INDEX [IX_PizzaToppings_PizzaId]
     ON [dbo].[PizzaToppings]([PizzaId] ASC);
 CREATE NONCLUSTERED INDEX [IX_PizzaToppings_IngredientId]
     ON [dbo].[PizzaToppings]([IngredientId] ASC);
-GO
-
--- Populate the database with example data (and some pizzas)
-
-INSERT INTO [dbo].[Pizzas]([Name])
-VALUES (N'Hawaian'), (N'Peperoni'), (N'Irish')
-
-INSERT INTO [dbo].[Ingredients]([Name])
-VALUES (N'Ham'), (N'Pineapple'), (N'Peperoni'), (N'Potatoes'), (N'Cabbage')
-
-INSERT INTO [dbo].[PizzaToppings]([PizzaId], [IngredientId])
-VALUES (1, 1), (1, 2), (2, 3), (3, 4), (3, 5)
 GO
